ff025253c1110cbe2c33308e179388aa2b59a749
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-07-04 12:26:57 +0300

    Anther fix.

--- backend/main.py
+++ backend/main.py
@@ -23,4 +23,4 @@ def read_hello():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8001))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=port) 
